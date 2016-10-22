5a6683c8f8fb3d19035cd18b664ff9c58f5f11c0
Suzii <z.dankovcikova@gmail.com>
2016-10-22 21:23:17 +0200

    Modify WebApi to return loweCamelCase jsons

--- src/Recipes/Recipes.Web/Global.asax.cs
+++ src/Recipes/Recipes.Web/Global.asax.cs
@@ -18,6 +18,7 @@ namespace Recipes.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            JsonFormattingConfig.ConfigureJsonFormatter();
         }
     }
 }
