ea2099925a1a8e00248f0dd146412727093aaf33
Suzii <z.dankovcikova@gmail.com>
2016-11-26 16:35:17 +0100

    Do not show pictures in similar recipes recommendations

--- src/WebApp/recipes-app/src/components/RecipeOverview.js
+++ src/WebApp/recipes-app/src/components/RecipeOverview.js
@@ -5,7 +5,7 @@ import { Link }from 'react-router';
 import { getTimeForUILonger } from './../utils/utils';
 import userActivityLogger from './../utils/userActivityLogger';
 
-const RecipeOverview = ({ id, title, cookTimeInMinutes, prepTimeInMinutes, imageUrl, isVegetarian, displayedRecipeId, recommendedBy, params }) => {
+const RecipeOverview = ({showImage, id, title, cookTimeInMinutes, prepTimeInMinutes, imageUrl, isVegetarian, displayedRecipeId, recommendedBy, params }) => {
     var isVegetarianClass = (isVegetarian)? 'veggie' : null;
     var recommenderTypes = recommendedBy.map(t => (<div key={t} className={`recommender-type recommender-type-${t}`}>&nbsp;</div>));
 
@@ -13,13 +13,19 @@ const RecipeOverview = ({ id, title, cookTimeInMinutes, prepTimeInMinutes, image
         console.log('Hurray, recipe with overlapping recommendations', displayedRecipeId, 'recommends', id);
     }
 
+    let picture;
+    if(showImage) {
+        picture = (
+            <div className="col-xs-3">
+            {recommenderTypes}
+            <img src={ imageUrl || noImage } className="img-responsive teaser-image" alt="recipe"/>
+        </div>);
+    }
+
     return (
         <div className="row recommendation">
-            <div className="col-xs-3">
-                {recommenderTypes}
-                <img src={ imageUrl || noImage } className="img-responsive teaser-image" alt="recipe"/>
-            </div>
-            <div className="col-xs-9">
+            { picture }
+            <div className={showImage? "col-xs-9" : "col-xs-12"}>
                 <p><Link to={`/Recipes/${id}`} className="title" onClick={() => userActivityLogger(displayedRecipeId, id, recommendedBy)}>{ title }</Link><span className={isVegetarianClass}></span></p>
                 <p> Cooking: { getTimeForUILonger(cookTimeInMinutes) }</p>
                 <p> Preparation: { getTimeForUILonger(prepTimeInMinutes) }</p>
