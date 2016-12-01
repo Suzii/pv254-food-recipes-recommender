2ae5d579f63f20d08253f18d94b8dca9f2fd81ef
Suzii <z.dankovcikova@gmail.com>
2016-12-01 22:01:00 +0100

    Sort ingredients into categories on UI

--- src/WebApp/recipes-app/src/pages/recipes/recipe/ingredients.js
+++ src/WebApp/recipes-app/src/pages/recipes/recipe/ingredients.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-const Ingredient = ({ id, subcategory, freeText, ingredient }) => {
+const isNonsenseString = (s) => !s || s === 'null';
+
+const Ingredient = ({ id, subCategory, freeText, ingredient }) => {
     var ingredientName = ingredient.name;
     let text = freeText.replace(ingredientName, `<b>${ingredientName}</b>`);
 
@@ -9,15 +11,46 @@ const Ingredient = ({ id, subcategory, freeText, ingredient }) => {
     );
 };
 
-const Ingredients = ({ ingredients }) => {
+const IngredientsInSubCategory = ({ subCategory, ingredients }) => {
     const ingredientsCode = (ingredients)
         ? <ul>{ ingredients.map((ingredient, index) => <Ingredient key={index} { ...ingredient } />) }</ul>
         : 'Nothing found... :(';
 
+    const subCategoryHeading = (isNonsenseString(subCategory))? 'Other' : subCategory;
+
+    return (
+        <div>
+            <h4>{ subCategoryHeading }</h4>
+            { ingredientsCode }
+        </div>
+    );
+}
+
+function sortIngredientsBySubCategories(ingredients) {
+    const ingredientsBySubCategory = {};
+    for(let i = 0; i < ingredients.length; i++) {
+        let ingredient = ingredients[i];
+        let ingredientsInSubcategory = ingredientsBySubCategory[ingredient.subCategory];
+        if(!ingredientsInSubcategory) {
+            ingredientsBySubCategory[ingredient.subCategory] = [];
+        }
+
+        ingredientsBySubCategory[ingredient.subCategory].push(ingredient);
+    }
+
+    return ingredientsBySubCategory;
+}
+
+const Ingredients = ({ ingredients }) => {
+    const ingredientsBySubCategories = sortIngredientsBySubCategories(ingredients);
+    const ingredientsBySubCategoryCode = (!ingredientsBySubCategories)? null : Object.keys(ingredientsBySubCategories).map((subCategory, index) => (
+        <IngredientsInSubCategory subCategory={subCategory} ingredients={ingredientsBySubCategories[subCategory]} key={index} />
+    ));
+
     return (
         <div className="ingredients" id="ingredients">
             <h2>Ingredients </h2>
-            { ingredientsCode }
+            { ingredientsBySubCategoryCode }
         </div>
     );
 };
